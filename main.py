f342b64f04ede7de04b83d8491ebb7b4c4851825
tomer1203 <tomer1shaked@gmail.com>
2021-12-16 16:03:41 +0200

    added noise and basic graphing

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ parser.add_argument('--index', type=int, default="25",
 parser.add_argument('--image', type=str,default="",
                     help='the path to customized image.')
 args = parser.parse_args()
-
+num_of_iterations = 300
 device = "cpu"
 if torch.cuda.is_available():
     device = "cuda"
@@ -33,73 +33,81 @@ tp = transforms.ToTensor()
 tt = transforms.ToPILImage()
 
 img_index = args.index
-gt_data = tp(dst[img_index][0]).to(device)
-
-if len(args.image) > 1:
-    gt_data = Image.open(args.image)
-    gt_data = tp(gt_data).to(device)
 
 
-gt_data = gt_data.view(1, *gt_data.size())
-gt_label = torch.Tensor([dst[img_index][1]]).long().to(device)
-gt_label = gt_label.view(1, )
-gt_onehot_label = label_to_onehot(gt_label)
+def test_image(img_index):
+    gt_data = tp(dst[img_index][0]).to(device)
+    if len(args.image) > 1:
+        gt_data = Image.open(args.image)
+        gt_data = tp(gt_data).to(device)
+    gt_data = gt_data.view(1, *gt_data.size())
+    gt_label = torch.Tensor([dst[img_index][1]]).long().to(device)
+    gt_label = gt_label.view(1, )
+    gt_onehot_label = label_to_onehot(gt_label)
 
-plt.imshow(tt(gt_data[0].cpu()))
+    plt.imshow(tt(gt_data[0].cpu()))
 
-from models.vision import LeNet, weights_init
-net = LeNet().to(device)
-
-
-torch.manual_seed(1234)
-
-net.apply(weights_init)
-criterion = cross_entropy_for_onehot
-
-# compute original gradient 
-pred = net(gt_data)
-y = criterion(pred, gt_onehot_label)
-dy_dx = torch.autograd.grad(y, net.parameters())
-
-original_dy_dx = list((_.detach().clone() for _ in dy_dx))
-
-# generate dummy data and label
-dummy_data = torch.randn(gt_data.size()).to(device).requires_grad_(True)
-dummy_label = torch.randn(gt_onehot_label.size()).to(device).requires_grad_(True)
-
-plt.imshow(tt(dummy_data[0].cpu()))
-
-optimizer = torch.optim.LBFGS([dummy_data, dummy_label])
+    from models.vision import LeNet, weights_init
+    net = LeNet().to(device)
 
 
-history = []
-for iters in range(300):
-    def closure():
-        optimizer.zero_grad()
+    torch.manual_seed(1234)
 
-        dummy_pred = net(dummy_data) 
-        dummy_onehot_label = F.softmax(dummy_label, dim=-1)
-        dummy_loss = criterion(dummy_pred, dummy_onehot_label) 
-        dummy_dy_dx = torch.autograd.grad(dummy_loss, net.parameters(), create_graph=True)
-        
-        grad_diff = 0
-        for gx, gy in zip(dummy_dy_dx, original_dy_dx): 
-            grad_diff += ((gx - gy) ** 2).sum()
-        grad_diff.backward()
-        
-        return grad_diff
-    
-    optimizer.step(closure)
-    if iters % 10 == 0: 
-        current_loss = closure()
-        print(iters, "%.4f" % current_loss.item())
-        history.append(tt(dummy_data[0].cpu()))
+    net.apply(weights_init)
+    criterion = cross_entropy_for_onehot
 
-plt.figure(figsize=(12, 8))
-for i in range(30):
-    plt.subplot(3, 10, i + 1)
-    plt.imshow(history[i])
-    plt.title("iter=%d" % (i * 10))
-    plt.axis('off')
+    # compute original gradient
+    pred = net(gt_data)
+    y = criterion(pred, gt_onehot_label)
+    dy_dx = torch.autograd.grad(y, net.parameters())
 
+    original_dy_dx = list((_.detach().clone() for _ in dy_dx))
+    #### adding noise!! ####
+    #original_dy_dx = [w_layer + torch.normal(mean = 0, std= 0.01,size = w_layer.shape) for w_layer in original_dy_dx]
+
+    # generate dummy data and label
+    dummy_data = torch.randn(gt_data.size()).to(device).requires_grad_(True)
+    dummy_label = torch.randn(gt_onehot_label.size()).to(device).requires_grad_(True)
+
+    plt.imshow(tt(dummy_data[0].cpu()))
+
+    optimizer = torch.optim.LBFGS([dummy_data, dummy_label])
+
+
+    history = []
+    for iters in range(num_of_iterations):
+        def closure():
+            optimizer.zero_grad()
+
+            dummy_pred = net(dummy_data)
+            dummy_onehot_label = F.softmax(dummy_label, dim=-1)
+            dummy_loss = criterion(dummy_pred, dummy_onehot_label)
+            dummy_dy_dx = torch.autograd.grad(dummy_loss, net.parameters(), create_graph=True)
+
+            grad_diff = 0
+            for gx, gy in zip(dummy_dy_dx, original_dy_dx):
+                grad_diff += ((gx - gy) ** 2).sum()
+            grad_diff.backward()
+
+            return grad_diff
+
+        optimizer.step(closure)
+        if iters % 10 == 0:
+            current_loss = closure()
+            print(iters, "%.4f" % current_loss.item())
+            history.append(tt(dummy_data[0].cpu()))
+
+    plt.figure(figsize=(12, 8))
+    for i in range(30):
+        plt.subplot(3, 10, i + 1)
+        plt.imshow(history[i])
+        plt.title("iter=%d" % (i * 10))
+        plt.axis('off')
+
+# l = []
+# for i in range(30):
+#     l.append(test_image(i))
+# print(l)
+# plt.hist([7 if (x>5) else x for x in l])
+test_image(img_index)
 plt.show()
