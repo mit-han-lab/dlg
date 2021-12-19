d53080e1de4ec26791de25510d7bf9402488635e
tomer1203 <tomer1shaked@gmail.com>
2021-12-19 20:19:26 +0200

    added advanced graphing

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from torchvision import models, datasets, transforms
 print(torch.__version__, torchvision.__version__)
 
 from utils import label_to_onehot, cross_entropy_for_onehot
+import random
+from torch.distributions.laplace import Laplace
 
 parser = argparse.ArgumentParser(description='Deep Leakage from Gradients.')
 parser.add_argument('--index', type=int, default="25",
@@ -36,7 +38,7 @@ tt = transforms.ToPILImage()
 img_index = args.index
 
 
-def test_image(img_index):
+def test_image(img_index,learning_iterations = 0,epsilon = 0):
     gt_data = tp(dst[img_index][0]).to(device)
     if len(args.image) > 1:
         gt_data = Image.open(args.image)
@@ -46,7 +48,7 @@ def test_image(img_index):
     gt_label = gt_label.view(1, )
     gt_onehot_label = label_to_onehot(gt_label)
 
-    plt.imshow(tt(gt_data[0].cpu()))
+    # plt.imshow(tt(gt_data[0].cpu()))
 
     from models.vision import LeNet, weights_init
     model = LeNet().to(device)
@@ -62,7 +64,7 @@ def test_image(img_index):
     iters, losses = [], []
     n = 0
 
-    while n<100:
+    while n<learning_iterations:
         y = model(gt_data)
         loss = criterion(y, gt_onehot_label)
         # dy_dx = torch.autograd.grad(y, net.parameters())
@@ -77,8 +79,6 @@ def test_image(img_index):
             print("Iteration: {0} Loss: {1} ".format(n, loss))
 
     ########################################
-
-
     # compute original gradient
     pred = model(gt_data)
     y = criterion(pred, gt_onehot_label)
@@ -87,12 +87,15 @@ def test_image(img_index):
     original_dy_dx = list((_.detach().clone() for _ in dy_dx))
     #### adding noise!! ####
     #original_dy_dx = [w_layer + torch.normal(mean = 0, std= 0.01,size = w_layer.shape) for w_layer in original_dy_dx]
-    #original_dy_dx = [w_layer+np.random.laplace(0,0.01,w_layer.shape) for w_layer in original_dy_dx]
+    #original_dy_dx = [w_layer+np.random.laplace(0,epsilon,w_layer.shape) for w_layer in original_dy_dx]
+    laplace_obj = Laplace(loc=0, scale=epsilon)
+    original_dy_dx = [w_layer+laplace_obj.sample(w_layer.shape) for w_layer in original_dy_dx]
+
     # generate dummy data and label
     dummy_data = torch.randn(gt_data.size()).to(device).requires_grad_(True)
     dummy_label = torch.randn(gt_onehot_label.size()).to(device).requires_grad_(True)
 
-    plt.imshow(tt(dummy_data[0].cpu()))
+    # plt.imshow(tt(dummy_data[0].cpu()))
 
     optimizer = torch.optim.LBFGS([dummy_data, dummy_label])
 
@@ -101,7 +104,7 @@ def test_image(img_index):
     current_loss = torch.Tensor([1])
     iters = 0
     #for iters in range(num_of_iterations):
-    while (current_loss.item()>0.001 and iters < 500):
+    while (current_loss.item()>0.003 and iters < 200):
 
         def closure():
             optimizer.zero_grad()
@@ -121,22 +124,49 @@ def test_image(img_index):
         optimizer.step(closure)
         if iters % 10 == 0:
             current_loss = closure()
-            print(iters, "%.4f" % current_loss.item())
-            history.append(tt(dummy_data[0].cpu()))
+        #     print(iters, "%.4f" % current_loss.item())
+        #     history.append(tt(dummy_data[0].cpu()))
         iters = iters + 1
+    # plt.figure(figsize=(12, 8))
+    # for i in range(round(iters / 10)):
+    #     plt.subplot(int(np.ceil(iters / 100)), 10, i + 1)
+    #     plt.imshow(history[i])
+    #     plt.title("iter=%d" % (i * 10))
+    #     plt.axis('off')
+    return current_loss.item()
 
-    plt.figure(figsize=(12, 8))
-    for i in range(round(iters/10)):
-        plt.subplot(int(np.ceil(num_of_iterations/100)), 10, i + 1)
-        plt.imshow(history[i])
-        plt.title("iter=%d" % (i * 10))
-        plt.axis('off')
 
 
 # l = []
-# for i in range(30):
-#     l.append(test_image(i))
+# for i in range(10):
+#     l.append(test_image(img_index,learning_iterations=500+50*i))
 # print(l)
-# plt.hist([7 if (x>5) else x for x in l])
-test_image(img_index)
+#plt.hist([7 if (x>5) else x for x in l])
+# plt.plot(l)
+image_number_list = [random.randrange(1, 1000, 1) for i in range(5)]
+epsilon_list = [0.1,0.08,0.06,0.03,0.01,0.003,0.001,0.0003,0.0001]
+print("chosen images: {0}".format(image_number_list))
+
+def run_dlg_tests(image_number_list,epsilon_list):
+    plt.xscale("log")
+    loss_per_epsilon_matrix = np.zeros([len(epsilon_list),len(image_number_list)])
+    for i,epsilon in enumerate(epsilon_list):
+        for j,n in enumerate(image_number_list):
+            loss_per_epsilon_matrix[i, j] = test_image(n, learning_iterations=0, epsilon=epsilon)
+            #loss_per_epsilon_matrix[i, j] = i+j
+        print("epsilon:{0} loss values:{1}".format(epsilon,loss_per_epsilon_matrix[i]))
+    with open('../output/epsilon_mat.npy', 'wb') as f:
+        np.save(f, loss_per_epsilon_matrix)
+    np.savetxt('../output/epsilon_mat.txt', loss_per_epsilon_matrix, fmt='%1.4e')
+    plt.plot(epsilon_list,np.mean(loss_per_epsilon_matrix,axis=1))
+    plt.title("dlg loss for different levels of laplace noise")
+    plt.grid(visible=True,axis="y")
+    plt.grid(visible=True,which='minor')
+    plt.xlabel("epsilon")
+    plt.ylabel("loss")
+
+#print(test_image(30, learning_iterations=0, epsilon=0.01))
+
+run_dlg_tests(image_number_list,epsilon_list)
 plt.show()
+
